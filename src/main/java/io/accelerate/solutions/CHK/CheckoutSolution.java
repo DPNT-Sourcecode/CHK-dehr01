bb8bdcb9326f79d7f82d079b1fa1966a0b6b804b
sbx_user1051 <sbx_user1051@169.254.3.59>
2025-03-26 16:20:59 +0000

    Wed Mar 26 16:20:59 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -4,18 +4,6 @@ import java.util.HashMap;
 
 public class CheckoutSolution {
 
-    /**
-     * @param n number of B's purchased
-     * @return cost of n B's with discounts applied
-     */
-    public Integer calculateBpricing(int n) {
-        if (n <= 0 ) {return 0;}
-        int price = 0;
-        price += 30 * (n % 2); // full price remainder
-        price += 45 * (n / 2); // discounts
-        return price;
-    }
-
     public Integer calculateFpricing(int n) {
         if (n <= 0 ) {return 0; }
         if (n < 3) { return 10 * n; }
@@ -26,7 +14,7 @@ public class CheckoutSolution {
     public Integer checkout(String skus) {
 
         // check string is valid
-        if (!skus.matches("[ABCDEF]*")) {
+        if (!skus.matches("[A-Z]*")) {
             return -1;
         }
 
@@ -39,9 +27,9 @@ public class CheckoutSolution {
 
         Shop shop = new Shop()
                 .addItem(
-                    new ShopItem('A', 50)
-                            .addDiscount(3, 130)
-                            .addDiscount(5, 200)
+                        new ShopItem('A', 50)
+                                .addDiscount(3, 130)
+                                .addDiscount(5, 200)
                 )
                 .addItem(
                         new ShopItem('B', 30)
@@ -54,12 +42,59 @@ public class CheckoutSolution {
                                 .addBuyXgetYfree(2, 1, 'B')
                 )
                 .addItem(
-                    new ShopItem('F', 10)
-                            .addBuyXgetYfree(2, 1, 'F')
-                );
+                        new ShopItem('F', 10)
+                                .addBuyXgetYfree(2, 1, 'F')
+                )
+                .addItem(new ShopItem('G', 20))
+                .addItem(
+                        new ShopItem('H', 10)
+                                .addDiscount(5, 45)
+                                .addDiscount(10, 80)
+                )
+                .addItem(new ShopItem('I', 35))
+                .addItem(new ShopItem('J', 60))
+                .addItem(
+                        new ShopItem('K', 80)
+                                .addDiscount(2, 150)
+                )
+                .addItem(new ShopItem('L', 90))
+                .addItem(new ShopItem('M', 15))
+                .addItem(
+                        new ShopItem('N', 40)
+                                .addBuyXgetYfree(3, 1, 'M')
+                )
+                .addItem(new ShopItem('O', 10))
+                .addItem(
+                        new ShopItem('P', 50)
+                                .addDiscount(5, 200)
+                )
+                .addItem(
+                        new ShopItem('Q', 30)
+                                .addDiscount(3, 80)
+                )
+                .addItem(
+                        new ShopItem('R', 50)
+                                .addBuyXgetYfree(3, 1, 'Q')
+                )
+                .addItem(new ShopItem('S', 30))
+                .addItem(new ShopItem('T', 20))
+                .addItem(
+                        new ShopItem('U', 40)
+                                .addBuyXgetYfree(3, 1, 'U')
+                )
+                .addItem(
+                        new ShopItem('V', 50)
+                                .addDiscount(2, 90)
+                                .addDiscount(3, 130)
+                )
+                .addItem(new ShopItem('W', 20))
+                .addItem(new ShopItem('X', 90))
+                .addItem(new ShopItem('Y', 10))
+                .addItem(new ShopItem('Z', 50));
 
 
         return shop.calculatePrice(freq);
     }
 }
+
 
