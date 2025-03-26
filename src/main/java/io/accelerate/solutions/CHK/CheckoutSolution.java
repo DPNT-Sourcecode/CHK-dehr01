2f8e819fa8ca8556089a3fc06adbe8ff4ad6feaa
sbx_user1051 <sbx_user1051@169.254.3.59>
2025-03-26 14:44:48 +0000

    Wed Mar 26 14:44:48 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -5,10 +5,22 @@ import io.accelerate.runner.SolutionNotImplementedException;
 import java.util.HashMap;
 
 public class CheckoutSolution {
+
+    /**
+     * @param n number of B's purchased
+     * @return cost of n B's with discounts applied
+     */
+    public Integer calculateBpricing(int n) {
+        int price = 0;
+        price += 30 * (n % 2); // full price remainder
+        price += 45 * (n / 2); // discounts
+        return price;
+    }
+
     public Integer checkout(String skus) {
 
         // check string is valid
-        if (!skus.matches("[ABCD]*")) {
+        if (!skus.matches("[ABCDE]*")) {
             return -1;
         }
 
@@ -21,31 +33,41 @@ public class CheckoutSolution {
             freq.put(item, previousFreq + 1);
         }
 
+
+        // sum over some items
+        // B prices
+        totalPrice += calculateBpricing(freq.get('B'));
+
         // sum over each item type
         for (char item : freq.keySet()) {
+            int n = freq.get(item);
+            int freeBcount = 0;
             switch (item) {
                 case 'A':
-                    int n = freq.get(item);
                     totalPrice += 200 * (n / 5);
                     n %= 5;
                     totalPrice += 130 * (n / 3);
                     n %= 3;
                     totalPrice += 50 * n;
                     break;
-                case 'B':
-                    totalPrice += 30 * (freq.get(item) % 2); // full price remainder
-                    totalPrice += 45 * (freq.get(item) / 2); // discounts
-                    break;
                 case 'C':
-                    totalPrice += 20 * freq.get(item);
+                    totalPrice += 20 * n;
                     break;
                 case 'D':
-                    totalPrice += 15 * freq.get(item);
+                    totalPrice += 15 * n;
                     break;
+                case 'E':
+                    totalPrice += 40 * n;
+                    freeBcount = n / 2;
             }
+//
+//            if (freq.get('B') - freeBcount > 0) {
+//                totalPrice -=
+//            }
         }
 
         return totalPrice;
     }
 }
+
 
