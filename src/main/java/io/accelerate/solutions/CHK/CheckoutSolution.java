87757b97ccd8ef7a6d93e302489bd99600b7ecaa
sbx_user1051 <sbx_user1051@169.254.3.59>
2025-03-26 14:11:35 +0000

    Wed Mar 26 14:11:35 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,39 @@ package io.accelerate.solutions.CHK;
 
 import io.accelerate.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        int totalPrice = 0;
+
+        // count how many of each item we are buying
+        HashMap<Character, Integer> frequencies = new HashMap<>();
+        for (char item : skus.toCharArray()) {
+            int previousFreq = frequencies.getOrDefault(item, 0);
+            frequencies.put(item, previousFreq + 1);
+        }
+
+        // sum over each item type
+        for (char item : frequencies.keySet()) {
+            switch (item) {
+                case 'A':
+                    
+                    totalPrice += 50 * frequencies.get(item);
+                    break;
+                case 'B':
+                    totalPrice += 30 * frequencies.get(item);
+                    break;
+                case 'C':
+                    totalPrice += 20 * frequencies.get(item);
+                    break;
+                case 'D':
+                    totalPrice += 15 * frequencies.get(item);
+                    break;
+            }
+        }
+
+        return totalPrice;
     }
 }
+
