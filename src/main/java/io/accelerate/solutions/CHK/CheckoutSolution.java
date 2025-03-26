ff8a5b516e6fb0f1806b27f771e6e1b099e4fe64
sbx_user1051 <sbx_user1051@169.254.3.59>
2025-03-26 14:20:35 +0000

    Wed Mar 26 14:20:35 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -14,19 +14,17 @@ public class CheckoutSolution {
             int previousFreq = freq.getOrDefault(item, 0);
             freq.put(item, previousFreq + 1);
         }
-
+        System.out.println(freq);
         // sum over each item type
         for (char item : freq.keySet()) {
             switch (item) {
                 case 'A':
-                    totalPrice += 50 * Math.floorMod(freq.get(item), 3);
-                    System.out.println(totalPrice + " " + Math.floorMod(freq.get(item), 3));
-                    totalPrice += 50 * freq.get(item) % 3;
-                    System.out.println(totalPrice + " " + (freq.get(item) % 3));
+                    totalPrice += 50 * (freq.get(item) / 3); // full price remainder
+                    totalPrice += 130 * (freq.get(item) % 3); // discounts
                     break;
                 case 'B':
-                    totalPrice += 45 * Math.floorMod(freq.get(item), 2);
-                    totalPrice += 30 * (freq.get(item) % 2);
+                    totalPrice += 30 * (freq.get(item) / 2); // full price remainder
+                    totalPrice += 45 * (freq.get(item) % 2); // discounts
                     break;
                 case 'C':
                     totalPrice += 20 * freq.get(item);
@@ -40,6 +38,7 @@ public class CheckoutSolution {
         return totalPrice;
     }
 }
+
 
 
 
