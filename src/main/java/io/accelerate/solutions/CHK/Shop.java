ae8438a6ec15f8716ce87f485cbb8bfc32c7be3d
sbx_user1051 <sbx_user1051@169.254.3.59>
2025-03-26 15:23:59 +0000

    Wed Mar 26 15:23:59 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/Shop.java
+++ src/main/java/io/accelerate/solutions/CHK/Shop.java
@@ -1,4 +1,18 @@
 package io.accelerate.solutions.CHK;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Shop {
+    List<ShopItem> items;
+
+    public Shop() {
+        items = new ArrayList<>();
+    };
+
+    public ShoppingCart addItem(ShopItem item) {
+        items.add(item);
+        return this;
+    }
 }
+
